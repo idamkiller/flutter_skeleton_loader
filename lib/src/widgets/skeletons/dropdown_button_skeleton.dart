57be0853e460d27ffc698dbdde1e5f369adee91d
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-04-18 12:11:47 -0500

    feat: require width and height parameters in DropdownButtonSkeleton for better control

--- lib/src/widgets/skeletons/dropdown_button_skeleton.dart
+++ lib/src/widgets/skeletons/dropdown_button_skeleton.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'base_skeleton.dart';
 
 class DropdownButtonSkeleton extends BaseSkeleton {
-  final double? width;
-  final double? height;
+  final double width;
+  final double height;
 
   const DropdownButtonSkeleton({
     super.key,
     required super.baseColor,
-    this.width,
-    this.height,
+    required this.width,
+    required this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width ?? 160,
-      height: height ?? 48,
+      width: width,
+      height: height,
       decoration: BoxDecoration(
         color: baseColor,
         borderRadius: BorderRadius.circular(8),
