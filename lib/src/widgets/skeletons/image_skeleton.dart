837548a79c1ddfc45ec95c6c87d0a7a041f5ce32
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-04-18 12:12:14 -0500

    feat: require width and height parameters in ImageSkeleton for better control

--- lib/src/widgets/skeletons/image_skeleton.dart
+++ lib/src/widgets/skeletons/image_skeleton.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'base_skeleton.dart';
 
 class ImageSkeleton extends BaseSkeleton {
-  final double? width;
-  final double? height;
+  final double width;
+  final double height;
 
   const ImageSkeleton({
     super.key,
     required super.baseColor,
-    this.width,
-    this.height,
+    required this.width,
+    required this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width ?? 100,
-      height: height ?? 100,
+      width: width,
+      height: height,
       decoration: BoxDecoration(
         color: baseColor,
         borderRadius: BorderRadius.circular(8),
