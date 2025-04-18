826bbbb778cc90356de73b84afa6997a286bfd5a
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-04-18 10:51:42 -0500

    feat: update SizedBoxSkeleton to require width, height, and isEmpty parameters for better control

--- lib/src/widgets/skeletons/sized_box_skeleton.dart
+++ lib/src/widgets/skeletons/sized_box_skeleton.dart
@@ -2,25 +2,30 @@ import 'package:flutter/material.dart';
 import 'base_skeleton.dart';
 
 class SizedBoxSkeleton extends BaseSkeleton {
-  final double? width;
-  final double? height;
+  final double width;
+  final double height;
+  final bool isEmpty;
 
   const SizedBoxSkeleton({
     super.key,
     required super.baseColor,
-    this.width,
-    this.height,
+    required this.width,
+    required this.height,
+    required this.isEmpty,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width ?? 100,
-      height: height ?? 10,
-      decoration: BoxDecoration(
-        color: baseColor,
-        borderRadius: BorderRadius.circular(8),
-      ),
+      width: width,
+      height: height,
+      decoration:
+          !isEmpty
+              ? BoxDecoration(
+                color: baseColor,
+                borderRadius: BorderRadius.circular(8),
+              )
+              : null,
     );
   }
 }
