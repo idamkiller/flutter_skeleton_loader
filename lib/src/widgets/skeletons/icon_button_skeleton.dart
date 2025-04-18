9da2b4e14a1a244d1cc4f101ec75c1284a564cc7
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-04-18 12:11:56 -0500

    feat: require width and height parameters in IconButtonSkeleton for better control

--- lib/src/widgets/skeletons/icon_button_skeleton.dart
+++ lib/src/widgets/skeletons/icon_button_skeleton.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'base_skeleton.dart';
 
 class IconButtonSkeleton extends BaseSkeleton {
-  final double? width;
-  final double? height;
+  final double width;
+  final double height;
 
   const IconButtonSkeleton({
     super.key,
     required super.baseColor,
-    this.width,
-    this.height,
+    required this.width,
+    required this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width ?? 24,
-      height: height ?? 24,
+      width: width,
+      height: height,
       decoration: BoxDecoration(color: baseColor, shape: BoxShape.circle),
     );
   }
