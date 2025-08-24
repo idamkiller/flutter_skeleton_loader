eefc2d280bf7da69d1e0b5773e12eceb6cfac39e
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-08-24 09:47:20 -0500

    feat: enhance SkeletonLoader with caching and error handling improvements

--- lib/flutter_skeleton_loader.dart
+++ lib/flutter_skeleton_loader.dart
@@ -1,1 +1,7 @@
 export 'skeleton_loader.dart';
+export 'src/config/skeleton_config.dart';
+export 'src/builders/skeleton_loader_builder.dart';
+export 'src/interfaces/skeleton_provider.dart';
+export 'src/utils/widget_validator.dart';
+export 'src/utils/optimized_skeleton_registry.dart';
+export 'src/widgets/skeletons/skeletons.dart';
